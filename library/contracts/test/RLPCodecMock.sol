725ef67c82a4dcfd33a73e9156a2ffaced8e0cdc
MoonKyu Song <mksong@parametacorp.com>
2023-05-19 18:33:08 +0900

    Consolidate RLPCodecMock.sol for moving RLPEncode/Decode.sol

--- library/contracts/test/RLPCodecMock.sol
+++ library/contracts/test/RLPCodecMock.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "../RLPDecode.sol";
-import "../RLPEncode.sol";
+import "../utils/RLPDecode.sol";
+import "../utils/RLPEncode.sol";
 
 contract RLPCodecMock {
     using RLPDecode for bytes;
